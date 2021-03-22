7a8ff456a99e98849b46c22811142899fb9589f5
Kaszcz <lukasz.kaszczyszyn@gmail.com>
2021-03-22 21:36:52 +0100

    loops

---
+++ src/Lesson02LogicalOperators.java
@@ -0,0 +1,19 @@
+public class Lesson02LogicalOperators {
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 23;
+        boolean isTall = x>=5;
+
+        //operator logiczny AND
+        if (isTall && y <= 20) {
+            System.out.println("prawda");
+        }
+
+        //operator logiczny OR
+        if ((x == 8) || (y < 20)) {
+            System.out.println("warunek spelniony");
+        }
+    }
+
+
+}
